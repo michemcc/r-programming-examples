8ba17fb5fc0a409109f55996f337aa6aa5004e98
Michael McCarthy <michemcc@outlook.com>
2020-04-26 19:48:18 -0400

    Create hello world file

---
+++ src/hello-world.R
@@ -0,0 +1,11 @@
+# Use the print() function to print the string "Hello World"
+print("Hello World")
+[1] "Hello World"
+
+# Suppress quotes in the output with the quote argument
+print("Hello World", quote = FALSE)
+[1] Hello World
+
+# For multiple strings, we can concatenate using paste()
+print(paste("Hello","World"))
+[1] "Hello World"
